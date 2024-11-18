de5b2cd7ccb775c1bd4c8a042dbda3ebdc7fbeb0
Gumiho 2504 <hemchanmetrey123@gmail.com>
2024-11-18 20:30:24 +0700

    laptop

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,73 +12,50 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-//        $car = Car::find(1);
-//        $car->features->abs = 0;
-//        dump($car->features,$car->primaryImage);
-//        $car->features->save();
+//        // select all car
+//        $cars = Car::all();
+//        // Select published Cars
+//        $cars = Car::where('published_at', '!=',null)->get();
+//
+//        // Select first car
+//        $car = Car::where('published_at', '!=',null)->first();
 
+        // select car by id
+ //       $car = Car::find(2);
+//        $cars = Car::orderBy('published_at', 'desc')
+//            ->limit(2)
+//            ->where('user_id' ,1)
+//            ->get();
+ //       dump($cars);
+    $carData = [
+        'maker_id'=>2,
+        'model_id'=>2,
+        'year'=>1999,
+        'price'=>1000,
+        'mileage'=> 100,
+        'car_type_id'=> 2,
+        'fuel_type_id'=>4,
+        'user_id'=> 1,
+        'city_id'=> 2,
+        'address'=> "Gumih",
+        'phone'=> "123455",
+        'description'=> "Phonix",
+        'published_at' => now()
+    ];
 
-//        $car = Car::find(1);
+    // Approach 1
+    Car::create($carData);
 
+    // Approach 2
+        $car2 = new Car();
+        $car2->fill($carData);
+    // Approach 3
+        $car3 = new Car($carData);
+        $car3->save();
 
-//        // Create new image
-//        $image = new CarImages(['image_path' =>'somtiing','position'=>'3']);
-//        $car->images()->save($image);
-//        dump($car->images);
-//        $car->images()->create(['image_path' =>'somtiing','position'=>'4']);
-//        $car->images()->saveMany([
-//            new CarImages(['image_path' =>'somtiing','position'=>'5']),
-//            new CarImages(['image_path' =>'somtiing','position'=>'6'])
-//        ]);
-//        $car->images()->createMany([
-//            ['image_path' =>'somtiing','position'=>'7'],
-//            ['image_path' =>'somtiing','position'=>'8'],
-//        ]);
-
-//        $car = Car::find(1);
-//        dd($car->carType);
-
-//        $car = Car::find(1);
-//        $carType = CarType::where('name','Jeep' )->first();
-
-        // change cartype id of the car
-//        $car->car_type_id = $carType->id;
-//        $car->save();
-
-//        $car->carType()->associate($carType);
-//        $car->save();
-      //  $cars = $carType->cars;
-      //  $cars = Car::whereBelongsTo($carType)->get();
-      //  dd($cars);
-
-
-///// Many to Many
-
-//        $car = Car::find(1);
-//        dd($car->favouredUsers);
-//        $user = User::find(1);
-//        dd($user->favouriteCars);
-//        $user = User::find(1);
-        // at favourite car to user by car id
-       // $user->favouriteCars()->attach([1,2]);
-        // delete all value in favourite car and create new one
-//        $user->favouriteCars()->sync([3]);
-
-        // delete favourite car from user
-///        $user->favouriteCars()->detach([1,2]);
-
-
-
-
-        ///##### FACTORY
-//        $maker =  Maker::factory()->count(4)->create();
-//        dd($maker);
-     $user =   User::factory()->count(10)->make([
-            'name' => 'metrey'
-        ]);
-        dd($user);
-
-        return view('home.index');
+        return view('home.index')
+            ->with('firstName','Home')
+            ->with('lastName','Home');
 
     }
 }
